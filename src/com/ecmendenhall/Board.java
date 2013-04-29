cf057fddf9acdc9c93cb5dac5393505ab65f19c6
Connor Mendenhall <ecmendenhall@gmail.com>
2013-04-29 15:26:34 -0500

    Board getRows method.

--- src/com/ecmendenhall/Board.java
+++ src/com/ecmendenhall/Board.java
@@ -1,5 +1,22 @@
 package com.ecmendenhall;
 
+import java.util.Arrays;
+
 public class Board {
     int[] squares = new int[9];
+
+    public boolean hasWin() {
+        return false;
+    }
+
+    public int[][] getRows() {
+        int[][] rows = new int[3][3];
+        for (int i=0; i < rows.length; i++) {
+            int firstsquare = i + 3;
+            int lastsquare = i + 6;
+            rows[i] = Arrays.copyOfRange(this.squares, firstsquare, lastsquare);
+        }
+        return rows;
+    }
+
 }
--- test/com/ecmendenhall/BoardTest.java
+++ test/com/ecmendenhall/BoardTest.java
@@ -3,6 +3,8 @@ package com.ecmendenhall;
 import com.ecmendenhall.Board;
 
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -12,15 +14,44 @@ import org.junit.Test;
 @RunWith(JUnit4.class)
 public class BoardTest {
 
+    private Board board;
+
+    @Before
+    public void setUp() {
+        board = new Board();
+    }
+
     @Test
     public void boardExists() {
-        Board board = new Board();
+        Board newboard = new Board();
     }
 
     @Test
     public void newBoardIsEmpty() {
-        Board newboard = new Board();
         int[] nineEmptySquares = new int[9];
-        Assert.assertArrayEquals("Board is not empty.", newboard.squares, nineEmptySquares);
+        Assert.assertArrayEquals("Board is not empty.", board.squares, nineEmptySquares);
+    }
+
+    @Test
+    public void noWinsOnNewBoard() {
+        Assert.assertFalse(board.hasWin());
+    }
+
+    @Test
+    public void boardHasRows() {
+        Assert.assertNotNull(board.getRows());
+    }
+
+    @Test
+    public void boardHasThreeRows() {
+        int[][] rows = board.getRows();
+        Assert.assertEquals(rows.length, 3);
+    }
+
+    @Test public void rowsHaveThreeSquares() {
+        int[][] rows = board.getRows();
+        for (int[] row : rows) {
+            Assert.assertEquals(row.length, 3);
+        }
     }
 }
