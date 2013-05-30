8d5c8c82042aa4383ff1b0da4cb466afca3c54a6
Connor Mendenhall <ecmendenhall@gmail.com>
2013-05-30 11:36:51 -0500

    Duplicated code: Create TicTacToeTest superclass

---
+++ test/com/cmendenhall/tests/TicTacToeTest.java
@@ -0,0 +1,19 @@
+package com.cmendenhall.tests;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class TicTacToeTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void cleanUp() {
+    }
+}
